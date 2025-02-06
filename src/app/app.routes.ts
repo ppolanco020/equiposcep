578e7a0a7b652fadcd4f4c3e9742e8966cfe6af3
ppolanco020 <68800368+ppolanco020@users.noreply.github.com>
2025-02-06 15:13:13 -0600

    crear usuario, forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { EquipoComponent } from './equipo/equipo.component';
 import { UsuarioCrearComponent } from './usuario.crear/usuario.crear.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
     {path:'welcome',component:BienvenidaComponent},
     {path:'usuario',component:UsuarioComponent},
     {path:'equipo',component:EquipoComponent},
-    {path:'crearUsuario',component:UsuarioCrearComponent}
+    {path:'crearUsuario',component:UsuarioCrearComponent},
+    {path:'dashboard',component:DashboardComponent}
 ];
